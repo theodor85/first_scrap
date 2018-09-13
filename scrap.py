d173a49fe353fffe8c91820e06e3748b887a54a3
Teddy Coder <qpwo@mail.ru>
2018-09-13 13:57:05 +0700

    Сделал обращение к случайному прокси и случайный User-Agent. Закончил извлечение имени ЖК и помещение его в табличку Excel

--- scrap.py
+++ scrap.py
@@ -1,7 +1,9 @@
 #-*-coding: utf-8 -*-
 
-from urllib.request import urlopen
+import urllib.request
+from urllib.request import urlopen, Request, ProxyHandler
 from bs4 import BeautifulSoup
+from random import choice
 import openpyxl
 
 # функция создаёт файл Excel и рисует шапку
@@ -20,9 +22,94 @@ def Head():
     sheet['C8'] = "адрес"
     sheet['D8'] = "цены"
 
-    wb.save("ex.xlsx")
+    sheet['E7'] = 'из блока с ценами (квардратные метры/цена/в продаже)'
+    sheet.merge_cells('E7:L7')
 
+    sheet['E8'] = "студии"
+    sheet['F8'] = "1 ком"
+    sheet['G8'] = "2 ком"
+    sheet['H8'] = "3 ком"
+    sheet['I8'] = "4 ком"
+    sheet['J8'] = "многокомнатные"
+    sheet['K8'] = "другие"
+    sheet['L8'] = "другие"
 
+    sheet['M7'] = 'акционный блок (загловок/текст/дедлайн)'
+    sheet.merge_cells('M7:Q7')
 
-Head()
+    sheet['M8'] = "акция 1"
+    sheet['N8'] = "акция 2"
+    sheet['O8'] = "акция 3"
+    sheet['P8'] = "акция 4"
+    sheet['Q8'] = "акция 5"
+
+    sheet['R7'] = 'расположение'
+    sheet.merge_cells('R7:AB7')
+
+    sheet['R8'] = "регион"
+    sheet['S8'] = "район"
+    sheet['T8'] = "локация"
+    sheet['U8'] = "метро + сколько минут пешком"
+    sheet['V8'] = "станция мцк"
+    sheet['W8'] = "жд станция+ колько минут пешком"
+    sheet['X8'] = "адрес"
+    sheet['Y8'] = "шоссе"
+    sheet['Z8'] = ""
+    sheet['AA8'] = ""
+    sheet['AB8'] = ""
+
+    return wb
+
+# функция выбирает один ЖК
+def OneJK(Workbook, URL, StringNum, proxy_name, useragent):
+
+    #указываем прокси-сервер
+    proxy = ProxyHandler({'http':proxy_name})
+    opener = urllib.request.build_opener(proxy)
+    urllib.request.install_opener(opener)
+
+    # открываем URL
+    req = Request(URL, headers = {'User-Agent':useragent})
+    html = urlopen(req)
+    bsObj = BeautifulSoup(html.read(), features="html.parser")
+
+    # получаем название ЖК
+    lst = bsObj.findAll("h1", {"class": "card_title", "itemprop": "name"})
+    # и записываем его в табличку
+    Cell = 'B'+str(StringNum)
+    Workbook.active[Cell] = lst[0].get_text()
+
+    print("Параметры запроса:")
+    print("\tUser-Agent:", useragent)
+    print("\tProxy:", proxy_name)
+    print("В ячейку", Cell, "записаны данные:", lst[0].get_text())
+    print("-"*40)
+
+#загружает список User-Agent
+def GetUserAgentsList(filename):
+    return open(filename).read().split('\n')
+
+#загружает список прокси-серверов
+def GetProxiesList(filename):
+    return open(filename).read().split('\n')
+
+#main
+
+UAList = GetUserAgentsList("useragents.txt")
+ProxiesList = GetProxiesList("proxy_list.txt")
+
+JKList = ['https://www.novostroy-m.ru/baza/zhk_flotiliya',
+    'https://www.novostroy-m.ru/baza/jk_mir_mitino',
+    'https://www.novostroy-m.ru/baza/jk_na_dushinskoy_ulitse',
+    'https://www.novostroy-m.ru/baza/apartkompleks_nahimov_nahimov',
+    'https://www.novostroy-m.ru/baza/jk_ryazanskiy_prospekt_2']
+
+wb = Head()
+
+i = 0
+for JK in JKList:
+    OneJK(wb, JK, 9+i, choice(ProxiesList), choice(UAList))
+    i+=1
+
+wb.save('ex.xlsx')
 print("Всё ОК!")
