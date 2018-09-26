3337461111a8d2746c9192312e27021995e53372
Teddy Coder <qpwo@mail.ru>
2018-09-26 16:01:42 +0700

    Осталась проблема с тем, что потоки не успевают завершиться, вслед за программой. Попытался сделать задержку, но не сработало.

--- pageslisthandler.py
+++ pageslisthandler.py
@@ -13,11 +13,19 @@ def PagesListHandler(URLList, OnePageHandlerClass):
 
     data = []
     Handlers = []
+    ProcessList = []
     q = Queue()
     for URL in URLList:
         Handler = OnePageHandlerClass(URL)
         p = Process(target=OnePageHandling, args=(Handler,q,))
+        ProcessList.append(p)
         p.start()
+    # здесь надо подождать,пока все потоки не отработают
+    while True:
+        for i in range(0, len(ProcessList)-1):
+            if ProcessList[i].is_alive():
+                continue
+        break
 
     for i in range(0, q.qsize()-1):
         data.append(q.get())
