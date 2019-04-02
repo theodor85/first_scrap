7f2bb5098dfeaa7fabadd30fdfe8b1d5416f0f5f
Teddy Coder <teddy_coder@mail.ru>
2019-04-02 11:42:02 +0700

    Убран декоратор класса. Вместо него - асбтрактный класс и наследование

--- pagehandler.py
+++ pagehandler.py
@@ -6,118 +6,134 @@ from bs4 import BeautifulSoup
 from random import choice
 from selenium import webdriver
 
-def PageHandlerDecorator(UserHandler):
-    """Декоратор класса.
-    В декорируемом классе необходимо определить:
+from abc import ABCMeta, abstractmethod
+
+# def PageHandlerDecorator(UserHandler):
+#     """Декоратор класса.
+#     В декорируемом классе необходимо определить:
+#         метод extract_data_from_html(self, soup=None, driver=None), в которой нужно прописать выборку
+#             данных из html-страницы
+#             soup - это объект BeautifulSoup
+#             driver - это объект selenium
+#         поле URL - инициализирующееся в конструкторе
+#         поле UseSelenium - инициализирующееся в конструкторе. Принимает значение Fаlse или True
+#     """
+
+class PageHandler(metaclass=ABCMeta):
+    """Абстракнтый класс для обработки одного URL.
+    В дочернем классе необходимо реализовать:
         метод extract_data_from_html(self, soup=None, driver=None), в которой нужно прописать выборку
             данных из html-страницы
             soup - это объект BeautifulSoup
             driver - это объект selenium
-        поле URL - инициализирующееся в конструкторе
-        поле UseSelenium - инициализирующееся в конструкторе. Принимает значение Fаlse или True
+        в конструкторе прописать вызов базового конструктора:
+            super().__init__()
+        в конструкторе определить поля:
+            поле URL - url-адрес страницы для обработки
+            поле UseSelenium - используется ли selenium (True) или BeautifulSoup (False)
     """
+    def __init__(self):
+        self.ProxiesList = self._get_proxies_list("proxy_list.txt")
+        self.UserAgentsList = self._get_user_agents_list("useragents.txt")
 
-    class PageHandler(UserHandler):
-        """Класс обрабатывает один URL."""
-        def __init__(self, URL):
-            super().__init__(URL)
-            self.ProxiesList = self._get_proxies_list("proxy_list.txt")
-            self.UserAgentsList = self._get_user_agents_list("useragents.txt")
+    @abstractmethod
+    def extract_data_from_html(self, soup=None, driver=None):
+        pass
 
-        #загружает список User-Agent
-        def _get_user_agents_list(self, filename):
-            return open(filename).read().strip().split('\n')
+    #загружает список User-Agent
+    def _get_user_agents_list(self, filename):
+        return open(filename).read().strip().split('\n')
 
-        #загружает список прокси-серверов
-        def _get_proxies_list(self, filename):
-            return open(filename).read().strip().split('\n')
+    #загружает список прокси-серверов
+    def _get_proxies_list(self, filename):
+        return open(filename).read().strip().split('\n')
 
-        def execute(self):
+    def execute(self):
 
-            if self.UseSelenium:
-                driver = self._get_selenium_driver()
-                self._open_url_with_selenium(driver)
-                data = self._get_data_from_page_with_selenium(driver)
-                driver.close()
-                return data
-            else:
-                return self._get_data_from_page_with_soup()
+        if self.UseSelenium:
+            driver = self._get_selenium_driver()
+            self._open_url_with_selenium(driver)
+            data = self._get_data_from_page_with_selenium(driver)
+            driver.close()
+            return data
+        else:
+            return self._get_data_from_page_with_soup()
 
 #****************** Методы для работы с Selenium *******************************
 
-        def _get_selenium_driver(self):
-            try:
-                driver = webdriver.Chrome(desired_capabilities=self._set_capabilities())
-            except Exception as err:
-                raise SelenimDriverException("Ошибка при открытии драйвера selenium!", self.URL, err)
-            return driver
+    def _get_selenium_driver(self):
+        try:
+            driver = webdriver.Chrome(desired_capabilities=self._set_capabilities())
+        except Exception as err:
+            raise SelenimDriverException("Ошибка при открытии драйвера selenium!", self.URL, err)
+        return driver
 
-        def _open_url_with_selenium(self, driver):
-            try:
-                driver.get(self.URL)
-            except Exception as err:
-                raise UrlOpenWithSeleniumException("Не удалось отрыть URL драйвером Selenium!", self.URL, err)
+    def _open_url_with_selenium(self, driver):
+        try:
+            driver.get(self.URL)
+        except Exception as err:
+            raise UrlOpenWithSeleniumException("Не удалось отрыть URL драйвером Selenium!", self.URL, err)
 
-        def _get_data_from_page_with_selenium(self, driver):
-            try:
-                data = self.extract_data_from_html(driver=driver)
-            except Exception as err:
-                raise ExtractDataWithSelenimException("Ошибка при извлечении данных со страницы с помощью selenium!", self.URL, err)
-            return data
+    def _get_data_from_page_with_selenium(self, driver):
+        try:
+            data = self.extract_data_from_html(driver=driver)
+        except Exception as err:
+            raise ExtractDataWithSelenimException("Ошибка при извлечении данных со страницы с помощью selenium!", self.URL, err)
+        return data
 
-        def _set_capabilities(self):
-            # используем случайный proxy
-            proxy_name = choice(self.ProxiesList)
-            capabilities = webdriver.DesiredCapabilities.CHROME.copy()
-            capabilities['proxy'] = {
-                'httpProxy':proxy_name,
-                'proxyType':'MANUAL',
-            }
-            return capabilities
+    def _set_capabilities(self):
+        # используем случайный proxy
+        proxy_name = choice(self.ProxiesList)
+        capabilities = webdriver.DesiredCapabilities.CHROME.copy()
+        capabilities['proxy'] = {
+            'httpProxy':proxy_name,
+            'proxyType':'MANUAL',
+        }
+        return capabilities
 
 #****************** Методы для работы с BeautifulSoup **************************
 
-        def _get_data_from_page_with_soup(self):
+    def _get_data_from_page_with_soup(self):
 
-            # открываем URL, получаем объект страницы и передаём его в BeautifulSoup
-            # выбираем необходимые данные
-            html = self._open_URL().read()
-            try:
-                data = self.extract_data_from_html( soup=BeautifulSoup(html, features="html.parser") )
-            except Exception as e:
-                raise Exception("Не удалось извлечь данные со страницы! \n\tURL: {URL} \n\tТекст ошибки: ".format(URL=self.URL) + str(e))
+        # открываем URL, получаем объект страницы и передаём его в BeautifulSoup
+        # выбираем необходимые данные
+        html = self._open_URL().read()
+        try:
+            data = self.extract_data_from_html( soup=BeautifulSoup(html, features="html.parser") )
+        except Exception as e:
+            raise Exception("Не удалось извлечь данные со страницы! \n\tURL: {URL} \n\tТекст ошибки: ".format(URL=self.URL) + str(e))
 
-            return data
+        return data
 
-        def _open_URL(self):
+    def _open_URL(self):
 
-            #указываем прокси-сервер, создаём объект запроса со случайным User-Agent
-            # и открываем URL
-            proxy = self._set_proxy()
-            UAName = choice(self.UserAgentsList)
-            req = Request(self.URL, headers={'User-Agent':UAName})
-            try:
-                html = urlopen(req)
-            except Exception as e:
-                raise Exception("""*** Ошибка при открытии URL функцией urlopen!
-                    URL:{URL}
-                    Прокси: {PROXY}
-                    User-Agent: {UA}
-                    Текст ошибки: """.format(
-                        URL=self.URL,
-                        PROXY=proxy.proxies['http'],
-                        UA=UAName)
-                            + str(e))
-            return html
+        #указываем прокси-сервер, создаём объект запроса со случайным User-Agent
+        # и открываем URL
+        proxy = self._set_proxy()
+        UAName = choice(self.UserAgentsList)
+        req = Request(self.URL, headers={'User-Agent':UAName})
+        try:
+            html = urlopen(req)
+        except Exception as e:
+            raise Exception("""*** Ошибка при открытии URL функцией urlopen!
+                URL:{URL}
+                Прокси: {PROXY}
+                User-Agent: {UA}
+                Текст ошибки: """.format(
+                    URL=self.URL,
+                    PROXY=proxy.proxies['http'],
+                    UA=UAName)
+                        + str(e))
+        return html
 
-        def _set_proxy(self):
-            proxy_name = choice(self.ProxiesList)
-            proxy = ProxyHandler({'http':proxy_name})
-            opener = build_opener(proxy)
-            install_opener(opener)
-            return proxy
+    def _set_proxy(self):
+        proxy_name = choice(self.ProxiesList)
+        proxy = ProxyHandler({'http':proxy_name})
+        opener = build_opener(proxy)
+        install_opener(opener)
+        return proxy
 
-    return PageHandler
+#    return PageHandler
 
 #****************************** Исключения *************************************
 
--- scrap.py
+++ scrap.py
@@ -1,71 +1,18 @@
 #-*-coding: utf-8 -*-
 
 import openpyxl
-from pagehandler import PageHandlerDecorator
+from pagehandler import PageHandler
 from listhandler import PagesListHandler
 from datetime import datetime
 from time import sleep
 import re
 
 
-# класс для использования в декораторе
-@PageHandlerDecorator
-class OneJKHandler(object):
-    def __init__(self, URL):
-        self.URL = URL
-        self.UseSelenium = False
-    # это та самая функция, которую нужно изменять для каждой html-страницы
-    def extract_data_from_html(self, soup=None, driver=None):
-
-        data = {}
-        data['jk_name'] = soup.find("h1", {"class": "card_title", "itemprop": "name"}).get_text()
-        data['address'] = soup.find("div", {"class": "info_item"}).get_text().strip()
-        data['price'] = ' '.join( soup.find("div", {"class": "descrition_price_item"}).get_text().strip().split() )
-        div_table = soup.find("div", {"id": "house_content"})
-        data['1_kom_metres'] = div_table.find("div", {"class":"card_row_item ws_nw lh_24 fs_16"}).get_text().strip()
-
-        return data
-
-@PageHandlerDecorator
-class ListPage(object):
-    def __init__(self, URL):
-        self.URL = URL
-        self.UseSelenium = True
-    def extract_data_from_html(self, soup=None, driver=None):
-
-        data = []
-
-        while True:
-            list_a = driver.find_elements_by_xpath( '//a[@class="pos_a z_i_1 d_b layout"]' )
-            for a in list_a:
-                href = a.get_attribute('href')
-                print("Найдена ссылка", href)
-                # если ссылка рекламная, то её пропускаем
-                if href.find('adfox') != -1:
-                    print("\tЭта ссылка рекламная!")
-                else:
-                    data.append(href)
-
-            # находим жлемент "Далее" и кликаем на нём. Если его нет (он не активен)
-            # то выходим из цикла
-            ul = driver.find_element_by_xpath( '//ul[@class="yiiPager"]' )
-            try:
-                li_next_hidden = ul.find_element_by_xpath('//li[@class="next hidden"]')
-                break
-            except:
-                li = ul.find_element_by_class_name('next')
-                a = li.find_element_by_tag_name('a')
-                a.click()
-                break  # для тестирования.
-                sleep(3)
-
-        return data
-
 # аренда
-@PageHandlerDecorator
-class LinksGetter(object):
+class LinksGetter(PageHandler):
 
     def __init__(self, URL):
+        super().__init__()
         self.URL = URL
         self.UseSelenium = False
 
