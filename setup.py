4550520eb3721da95d152427dd9f8d8c0c261171
Teddy Coder <teddy_coder@mail.ru>
2019-04-24 11:15:39 +0700

    Изменена структура каталогов проекта

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='firstscrap',
+    version='0.1.0',
+    packages=find_packages(),
+    author='Teddy Coder',
+    author_email='fedor_coder@mail.ru',
+    url='https://github.com/theodor85/first_scrap',
+)
